2c422bc90049e66ddd76a83e4b2c63f079b23b08
chinaza-max <mosesogbonna68@gmail.com>
2024-04-10 12:56:04 +0100

    test changes

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const domain= ''//'https://webrtc-ymot.onrender.com'
 
 console.log(location.hostname)
 const socket=io('https://webrtc-ymot.onrender.com/')
-const mypeer=new Peer(undefined,{host:location.hostname/*, port: location.port || (location.protocol === 'https:' ? 443 : 80)*/, path:'/peerjs'})
+const mypeer=new Peer(undefined,{host:location.hostname/*, port: location.port || (location.protocol === 'https:' ? 443 : 80)*/, path:'/'})
  
 const Ids={}
 const peers={}
